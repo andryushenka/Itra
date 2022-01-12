ddbb8113a1bb503d5f070f8020044123987b4d11
Andrew <93209829+andryushenka@users.noreply.github.com>
2022-01-12 13:56:11 +0300

    finish?

--- mysql/init-db.sh
+++ mysql/init-db.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+service mysql stop 
+service mysql start
 mysql -u root <<MYSQL_SCRIPT
 CREATE DATABASE db;
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON db.* TO 'adim'@'localhost';
+GRANT ALL PRIVILEGES ON db.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT

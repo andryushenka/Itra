d52815e1842747466ac71406da053dbaf3d7b0d6
Andrew <93209829+andryushenka@users.noreply.github.com>
2022-01-17 13:31:57 +0300

    credentials with arg

--- mysql/init-db.sh
+++ mysql/init-db.sh
@@ -4,7 +4,7 @@ service mysql stop
 service mysql start
 mysql -u root <<MYSQL_SCRIPT
 CREATE DATABASE db;
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON db.* TO 'admin'@'localhost' WITH GRANT OPTION;
+CREATE USER '$1'@'localhost' IDENTIFIED BY '$2';
+GRANT ALL PRIVILEGES ON db.* TO '$1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
